1de42f79b914b2bdfbca0f69a5019305a818f3e1
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-06-08 20:10:49 +0200

    first commit

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,197 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flower_app/shared/data_from_firestore.dart';
+import 'package:flower_app/shared/user_img_from_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+import 'package:flower_app/shared/colors.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final credential = FirebaseAuth.instance.currentUser;
+  File? imgPath;
+
+  uploadImage2Screen() async {
+    final pickedImg =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    try {
+      if (pickedImg != null) {
+        setState(() {
+          imgPath = File(pickedImg.path);
+        });
+      } else {
+        print("NO img selected");
+      }
+    } catch (e) {
+      print("Error => $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          TextButton.icon(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              if (!mounted) return;
+              Navigator.pop(context);
+            },
+            label: Text(
+              "logout",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            icon: Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+          )
+        ],
+        backgroundColor: appbarGreen,
+        title: Text("Profile Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(22.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(125, 78, 91, 110),
+                  ),
+                  child: Stack(
+                    children: [
+                      imgPath == null
+                          ? ImgUser()
+                          : ClipOval(
+                              child: Image.file(
+                                imgPath!,
+                                width: 145,
+                                height: 145,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                      Positioned(
+                        left: 99,
+                        bottom: -10,
+                        child: IconButton(
+                          onPressed: () {
+                            uploadImage2Screen();
+                          },
+                          icon: const Icon(Icons.add_a_photo),
+                          color: Color.fromARGB(255, 94, 115, 128),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 33,
+              ),
+              Center(
+                  child: Container(
+                padding: EdgeInsets.all(11),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 131, 177, 255),
+                    borderRadius: BorderRadius.circular(11)),
+                child: Text(
+                  "Info from firebase Auth",
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                ),
+              )),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    "Email: ${credential!.email}      ",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    //
+
+                    "Created date:   ${DateFormat("MMMM d, y").format(credential!.metadata.creationTime!)}   ",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    //
+                    "Last Signed In: ${DateFormat("MMMM d, y").format(credential!.metadata.lastSignInTime!)}  ",
+                    style: TextStyle(
+                      fontSize: 17,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Center(
+                child: TextButton(
+                    onPressed: () {
+                      credential!.delete();
+
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Delete User",
+                      style: TextStyle(fontSize: 18),
+                    )),
+              ),
+              SizedBox(
+                height: 22,
+              ),
+              Center(
+                  child: Container(
+                      padding: EdgeInsets.all(11),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 131, 177, 255),
+                          borderRadius: BorderRadius.circular(11)),
+                      child: Text(
+                        "Info from firebase firestore",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ))),
+              GetDataFromFirestore(
+                documentId: credential!.uid,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
